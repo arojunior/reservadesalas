241af8b40932550012c0a1269bc1358910e2a8ee
Junior Oliveira <arojunior@gmail.com>
2017-05-19 16:46:45 -0300

    boilerplate config

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,13 @@ import {render} from 'react-dom'
 import {Router, browserHistory} from 'react-router'
 
 import Routes from './routes'
-import './assets/index.css'
 
 import {Provider} from 'react-redux'
-import {store} from  './modules'
+import {store} from './modules'
 
 render(
-    <Provider store={store}>
-      <Router history={browserHistory} routes={Routes} />
-    </Provider>,
+  <Provider store={store}>
+    <Router history={browserHistory} routes={Routes} />
+  </Provider>,
   document.getElementById('root')
 )
