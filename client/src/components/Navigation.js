e03e5b63a310c3ffd807fb4fa2aeb4af2df28d80
Junior Oliveira <arojunior@gmail.com>
2017-05-22 15:47:59 -0300

    navigaton bar style added

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,17 +1,35 @@
 import React from 'react'
-import { Link } from 'react-router'
-
-const styles = {
-  link: {
-    display: 'inline-block',
-    margin: '0.5em'
-  }
-}
+import {Link} from 'react-router'
 
 const Navigation = () => (
-  <nav>
-      <Link to='/' style={ styles.link }>Home</Link>
-      <Link to='/about' style={ styles.link }>About</Link>
+  <nav className="navbar navbar-inverse">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <button
+          type="button"
+          className="navbar-toggle collapsed"
+          data-toggle="collapse"
+          data-target="#navbar"
+          aria-expanded="false"
+          aria-controls="navbar">
+          <span className="sr-only">Toggle navigation</span>
+          <span className="icon-bar" />
+          <span className="icon-bar" />
+          <span className="icon-bar" />
+        </button>
+        <a className="navbar-brand" href="#">Reserva de Salas</a>
+      </div>
+      <div id="navbar" className="navbar-collapse collapse">
+        <ul className="nav navbar-nav navbar-right">
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/logout">Logout</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
   </nav>
 )
 
