29a18a756187c5ce898bd262221e838fd1fd6c08
Junior Oliveira <arojunior@gmail.com>
2017-05-23 16:47:36 -0300

    module Locais added

---
+++ client/src/modules/Locais/index.js
@@ -0,0 +1,14 @@
+const initialState = {
+  data: [
+    {
+      id: 1,
+      nome: 'Local 1'
+    },
+    {
+      id: 2,
+      nome: 'Local 2'
+    }
+  ]
+}
+
+export default (state = initialState, action) => state
