3c36ff2640ee6765b29da0f2094bccf68152972f
Junior Oliveira <arojunior@gmail.com>
2017-05-24 18:01:00 -0300

    usuarios model created

--- server-node/src/database/mongo.js
+++ server-node/src/database/mongo.js
@@ -1,6 +1,6 @@
-import mongoose from 'mongoose'
+const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://root:root@ds151951.mlab.com:51951/reserva-de-salas')
 mongoose.Promise = require('bluebird')
 
-export const db = mongoose.connection
+module.exports = mongoose.connection
